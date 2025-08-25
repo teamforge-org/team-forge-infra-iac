e38c6c3d258cb6b6933e7d1b9670ae837eba8406
Bellorico323 <mabo.orico@gmail.com>
2025-08-25 17:02:08 -0300

    new: bucket account service

--- storage.tf
+++ storage.tf
@@ -18,3 +18,14 @@ resource "google_storage_bucket_iam_member" "public_reader" {
   role   = "roles/storage.objectViewer"
   member = "allUsers"
 }
+
+resource "google_service_account" "writer_service_account" {
+  account_id   = "${terraform.workspace}-bucket-writer"
+  display_name = "Service Account with write access to the team-forge bucket"
+}
+
+resource "google_storage_bucket_iam_member" "writer_iam" {
+  bucket = google_storage_bucket.team-forge-bucket.name
+  role   = "roles/storage.objectAdmin"
+  member = "serviceAccount:${google_service_account.writer_service_account.email}"
+}
